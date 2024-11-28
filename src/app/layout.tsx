66ee87fb139eeca2b109e3ef4338dd14458ebbbe
Cassiano26 <candidocassiano2@gmail.com>
2024-11-28 14:58:48 -0300

    style: add navbar component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Montserrat } from 'next/font/google'
+import { NavBar } from "../items/navbar";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -18,7 +19,8 @@ export default function RootLayout({
         className={`antialiased`}
       >
         <div className="w-[1728px] ml-auto mr-auto">
-        {children}
+          <NavBar />
+          {children}
         </div>
       </body>
     </html>
